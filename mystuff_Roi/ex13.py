07d5872362f40b8f9bbe05a9c9dfe22fafda0d2c
jroypeterson <jroypeterson@gmail.com>
2017-10-10 08:32:19 -0400

    Roivant exercises

---
+++ mystuff_Roi/ex13.py
@@ -0,0 +1,12 @@
+from sys import argv
+script, first, second, third = argv
+
+
+print "The script is called:", script
+print "The first variable is called:", first
+print "The second variable is called:", second
+print " THe third variable is called:", third
+
+age = raw_input("how old are you?")
+
+print age
---
+++ mystuff_Roi/ex4.py
@@ -0,0 +1,5 @@
+cars = 100
+space_in_a_Car = 4.0
+
+print "There are", cars, "cars available."
+print "There is", space_in_a_Car, "space in a car" 
